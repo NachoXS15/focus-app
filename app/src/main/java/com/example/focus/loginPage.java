8ac5dbfe3cd8427df7b86d77e72560e7e20d7184
NachoXS <pantojaignacio642@gmail.com>
2023-02-08 20:23:46 -0300

    entry page added

---
+++ app/src/main/java/com/example/focus/loginPage.java
@@ -0,0 +1,15 @@
+package com.example.focus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+}
