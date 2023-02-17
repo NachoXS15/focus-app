c812f9d8a82675088724264a05ead7aa360b108d
NachoXS <pantojaignacio642@gmail.com>
2023-02-17 02:34:02 -0300

    options menu design done

--- app/src/main/java/com/example/focus/mainPage.java
+++ app/src/main/java/com/example/focus/mainPage.java
@@ -2,7 +2,9 @@ package com.example.focus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class mainPage extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class mainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    //go
+    public void goOptions (View view){
+        Intent avanzarOptions = new Intent(this, optionsMenu.class);
+        startActivity(avanzarOptions);
     }
 }
--- app/src/main/java/com/example/focus/optionsMenu.java
+++ app/src/main/java/com/example/focus/optionsMenu.java
@@ -2,7 +2,9 @@ package com.example.focus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class optionsMenu extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class optionsMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_menu);
+    }
+
+    public void goBack(View view){
+         Intent volver = new Intent(this, mainPage.class);
+         startActivity(volver);
     }
 }
